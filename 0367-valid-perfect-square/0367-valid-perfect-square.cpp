af67e3b0fe02668fd20abeecb4b8673a537e54f7
indra mohan kumar <deviavlisha@gmail.com>
2026-03-20 21:36:05 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.47%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 1) return true;
+
+        long long low = 1, high = num;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+
+            if (mid * mid == num) return true;
+
+            else if (mid * mid < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
