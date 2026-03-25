596f29107d199d7e2c85cd424b569f6966cf14e0
indra mohan kumar <deviavlisha@gmail.com>
2026-03-25 18:14:39 +0530

    Time: 9 ms (54.02%), Space: 11.6 MB (99.83%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode*temp=head;
-        map<ListNode*,int>mpp;
-        while(temp!=NULL){
-            if (mpp.find(temp)!=mpp.end()){
+        ListNode*fast=head;
+        ListNode*slow=head;
+        while(fast!=NULL&&fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
                 return true;
+
             }
-            mpp[temp]=1;
-            temp=temp->next;
         }
         return false ;
     }
