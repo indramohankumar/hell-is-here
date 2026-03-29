0403aaa9e7d7a3367499604372589aa5868e3e6a
indra mohan kumar <deviavlisha@gmail.com>
2026-03-29 10:17:47 +0530

    Time: 10 ms (46.61%), Space: 11.5 MB (23.28%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,22 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-       ListNode*temp=head;
-       map<ListNode*,int>mpp;
-       while(temp!=NULL){
-        if(mpp.find(temp)!=mpp.end()){
-            return temp;
+       ListNode*slow=head;
+       ListNode*fast=head;
+       while(fast!=NULL&&fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            slow=head;
+            while(slow!=fast){
+                slow=slow->next;
+                fast=fast->next;
+
+            }
+            return slow;
         }
-        mpp[temp] = 1;
-        temp=temp->next;
-       } 
-       return NULL;
+     
+       }
+          return NULL;
     }
 };
