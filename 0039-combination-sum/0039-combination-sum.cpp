bad67f479493486d866af61f86945c243b05029f
indra mohan kumar <deviavlisha@gmail.com>
2026-06-26 10:49:45 +0530

    Time: 7 ms (25.33%), Space: 15.8 MB (30.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+set<vector<int>>st;
 void getallcombination(vector<int>&arr,int idx,int tar,vector<int>&combination,vector<vector<int>>&ans){
     if(idx==arr.size()||tar<0) return ;
     if(tar==0) {
-        ans.push_back({combination});
+        st.insert(combination);
+        //ans.push_back({combination});
         return ;
     }
     combination.push_back(arr[idx]);
-getallcombination(arr,idx,tar-arr[idx],combination,ans);//single
-
+getallcombination(arr,idx+1,tar-arr[idx],combination,ans);//single
+getallcombination(arr,idx,tar-arr[idx],combination,ans);//multiple
 combination.pop_back();
 getallcombination(arr,idx+1,tar,combination,ans);//exclusion
 
@@ -18,6 +20,6 @@ getallcombination(arr,idx+1,tar,combination,ans);//exclusion
         vector<int>combination;
         vector<vector<int>>ans;
         getallcombination(arr,0,target,combination,ans);
-        return ans;
+           return vector<vector<int>>(st.begin(), st.end());
     }
 };
